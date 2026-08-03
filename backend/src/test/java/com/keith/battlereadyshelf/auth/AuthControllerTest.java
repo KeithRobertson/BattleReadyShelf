bfd45268c9d41ea8a3892e688545925b6b053f4f
keith <robertsonk91@gmail.com>
2026-08-03 12:40:00 +0100

    Add mockito extension to test

--- backend/src/test/java/com/keith/battlereadyshelf/auth/AuthControllerTest.java
+++ backend/src/test/java/com/keith/battlereadyshelf/auth/AuthControllerTest.java
@@ -9,10 +9,13 @@ import com.keith.battlereadyshelf.generated.model.UserDto;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.UUID;
 
+@ExtendWith(MockitoExtension.class)
 class AuthControllerTest {
 
     @Mock private AuthService authService;
