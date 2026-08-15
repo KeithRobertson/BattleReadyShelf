d0a8f38cf0c0c785f54c09ac2a54425871c0b702
keith <robertsonk91@gmail.com>
2026-08-15 18:06:01 +0100

    Batch requests to fix N+1 query slowdown

--- backend/src/main/java/com/keith/battlereadyshelf/collectionmodel/CollectionModelRepository.java
+++ backend/src/main/java/com/keith/battlereadyshelf/collectionmodel/CollectionModelRepository.java
@@ -1,5 +1,6 @@
 package com.keith.battlereadyshelf.collectionmodel;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -8,6 +9,7 @@ import java.util.List;
 import java.util.UUID;
 
 public interface CollectionModelRepository extends JpaRepository<CollectionModelEntity, UUID> {
+    @EntityGraph(attributePaths = "modelDefinition")
     List<CollectionModelEntity> findAllByArmyCollectionId(UUID armyCollectionId);
 
     @Query(
